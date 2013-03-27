9d33596d808fe403eb06b0de619fa00e3ef52a1f
Hershal Bhave <hershal.bhave@gmail.com>
2013-03-27 03:07:21 -0500

    updated index, added steepestdescent algorithm, worked on 368k hw

---
+++ Homeworks/HW8/steepest.m
@@ -0,0 +1,57 @@
+#!/usr/bin/octave
+# Created by Hershal Bhave on 3/25/13
+# For M368K HW8, § 10.1 Number 1
+# Written in GNU Octave
+#
+# Description: Uses the Steepest Descent Method to minimize the
+# function f given an initial approximation x and the maximum number
+# of steps n
+#
+
+function x = steepest(x,f,n)
+
+  tol = 10^-6;
+  k=1;
+  gg=@(x)(sum(f(x).^2));
+  while k<n
+
+    g(2)=gg(x);
+    z=2*jacob(x,f)'*f(x);
+    z0=norm(z);
+
+    z=z/z0;
+    alpha(2)=0;
+    alpha(4)=1;
+    g(4)=gg(x-alpha(4)*z);
+
+    while g(4)>g(2)
+      alpha(4)=alpha(4)/2
+      g(4)=gg(x-alpha(4)*z);
+      if alpha(4)<tol/2
+	fprintf("No likely improvement\n");
+	k=n;
+      endif
+    endwhile
+
+    alpha(3)=alpha(4)/2;
+    g(3)=gg(x-alpha(3)*z);
+    h1=(g(3)-g(2))/alpha(3);
+    h2=(g(4)-g(3))/(alpha(4)-alpha(3));
+    h3=(h2-h1)/(alpha(4));
+
+    alpha(1)=0.5*(alpha(3)-h1/h3);
+    g(1)=gg(x-alpha(1)*z);
+
+    [gmin,minidx] = min(g([1:4]))
+
+    x=x-alpha(minidx)*z;
+
+    if abs(gmin-g(2)) < tol
+       k=n;
+    endif
+
+    k++;
+
+  endwhile
+
+endfunction
