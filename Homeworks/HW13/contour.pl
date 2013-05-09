d67f120ba241b67a5219dd9e6bd887490ba27c32
Hershal Bhave <hershal.bhave@gmail.com>
2013-05-09 10:04:31 -0500

    finished m368k hw13

--- Homeworks/HW13/contour.pl
+++ Homeworks/HW13/contour.pl
@@ -52,16 +52,10 @@ set pm3d map
 set palette defined ( 0 "#000090", 1 "#000fff", 2 "#0090ff",\\
                       3 "#0fffee", 4 "#90ff70", 5 "#ffee00",\\
                       6 "#ff7000", 7 "#ee0000", 8 "#7f0000")
-
 splot '$outFile'
 
 GNUEOF
-
 PERLEOF
-  
   system("$plotcmd");
 }
-
 printf("done\n");
-
-
